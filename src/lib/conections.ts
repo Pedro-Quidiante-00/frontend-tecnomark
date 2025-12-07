4cd7805702816cc7478dc579c3fb90ea5f4766c5
Pedro Quidiante <pedroalejandro.quidiante@alumnos.ulagos.cl>
2025-12-06 22:08:06 -0300

    c-2

--- src/lib/conections.ts
+++ src/lib/conections.ts
@@ -1,2 +1,19 @@
-const STRAPI_URL = import.meta.env.PUBLIC_STRAPI_URL;
-const STRAPI_TOKEN = import.meta.env.PUBLIC_STRAPI_TOKEN;
+const strapiUrl = import.meta.env.PUBLIC_STRAPI_URL || 'https://astro-backend-yryd.onrender.com/api';
+
+export const STRAPI_BASE_URL = strapiUrl.replace('/api', '');
+export const STRAPI_URL = strapiUrl;
+
+export const fetchFromAPI = async (slug: string) => {
+    try {
+        const response = await fetch(`${STRAPI_URL}/${slug}?populate=*`);
+        const json = await response.json();
+
+        if(!response.ok) {
+            throw new Error('El error', json.message);
+        }
+
+        return json.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
